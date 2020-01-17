64faf39848770afba502d1af36d3bfd3b500ad07
Rojax98 <jackroper.smith@gmail.com>
2020-01-17 18:29:48 +0000

    Project
    added

---
+++ physics puzzle jam/Assets/gravChange.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gravChange : MonoBehaviour {
+
+	public float gravNum;
+
+	public TextMesh gravText;
+
+	public bool gravTog;
+
+	// Use this for initialization
+	void Start () {
+		//gravTog = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gravText.text = ("Gravity: " + gravNum);
+
+		gravNum = this.GetComponent<scaleChange> ().scaleNum;
+
+
+		if (this.GetComponent<scaleChange> ().scaleNum < 0.3f) {
+			gravNum = 0;
+
+		}
+
+		this.GetComponent<Rigidbody2D> ().gravityScale = gravNum;
+
+
+
+		if(Input.GetKeyDown(KeyCode.R)){
+			SceneManager.LoadScene(0);
+		}
+
+
+		//if (Input.GetKeyDown (KeyCode.G)) {
+
+			//if (gravTog == true) {				
+
+				//gravTog = false;
+			//}
+
+			//else if (gravTog == false) {
+
+				//gravTog = true;
+			//}
+		//}
+
+		//if (gravTog == true) {
+			//this.GetComponent<Rigidbody2D> ().gravityScale = 1;
+			//gravText.text = "Gravity: On";
+		//}
+
+		//if (gravTog == false) {
+			//this.GetComponent<Rigidbody2D> ().gravityScale = 0;
+			//gravText.text = "Gravity: Off";
+		//}
+
+	}
+}
---
+++ physics puzzle jam/Assets/scaleChange.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scaleChange : MonoBehaviour {
+
+	public float scaleNum = 0.5f;
+	Rigidbody2D body2D;
+	public float maxScale;
+	public bool hit;
+
+	// Use this for initialization
+	void Start () {
+
+		scaleNum = 0.2f;
+
+		body2D = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (scaleNum < 0.1f) {
+			scaleNum = 0.1f;
+
+		}
+		if (scaleNum > 1f) {
+
+
+			scaleNum = 1f;
+
+		}
+
+		
+
+		var S = Input.GetAxis ("Mouse ScrollWheel");
+
+		//print (S);
+
+		body2D.transform.localScale = new Vector3 (scaleNum, scaleNum, 0);
+
+
+		if (S < 0f) {
+
+			scaleNum -= 0.1f;
+
+		}
+			
+
+
+
+		if (S > 0f) {
+
+			scaleNum += 0.1f;
+
+		}
+
+
+
+	}
+		
+}
+
